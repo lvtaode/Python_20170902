4f4aa9b7c58ac1d9050a634dbbb40ca47d1a0517
thu <mingfei.net@gmail.com>
2018-06-07 22:21:49 +0800

    Initial commit

---
+++ ioTest.py
@@ -0,0 +1,14 @@
+print('Hi...')
+print("Hi...")
+
+print('a', 'b', 'c', 'd')
+
+print(100 + 200)
+
+print('100 + 200 = ', 100 + 200)
+
+# Java SE Scanner
+
+name = input('input your name: ')
+
+print('Hello,', name, '!')
