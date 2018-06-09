fa464fcaf3e7f53bba51a3c0485794d2d21a494f
thu <mingfei.net@gmail.com>
2018-06-09 10:38:26 +0800

    Update

---
+++ strTest.py
@@ -0,0 +1,25 @@
+s1 = 'a'
+
+s2 = '一'
+
+print(ord(s1))
+
+print(ord(s2))
+
+print(0x4E00)
+
+print(chr(19971))
+
+print(chr(98))
+
+s3 = 'abc一二三'
+
+print(len(s3))
+
+# format: Hello xxx, today is yyy
+
+print('Hello %s, today is %s' % ('Tom', 'Sat.'))
+
+print('Hello %s' % 'Jerry')
+
+# %s: str; %f: float; %d: digit; %x: hex
