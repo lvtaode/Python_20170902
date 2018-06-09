16bf62b12a3489fe1456f863f9ad0ff9f3b46d42
thu <mingfei.net@gmail.com>
2018-06-09 11:06:43 +0800

    Update

---
+++ listTest.py
@@ -0,0 +1,44 @@
+#  list
+
+list1 = ['a', 'b', 'c', 'd']
+
+print(list1)
+
+print(len(list1))
+
+print(list1[0])
+
+print(list1[len(list1) - 1])
+
+print(list1[-1])
+
+print(list1[-4])
+
+list1.append('e')
+
+print(list1)
+
+list1.insert(1, 'x')
+
+print(list1)
+
+print(list1.pop())
+
+print(list1)
+
+print(list1.pop(1))
+
+print(list1)
+
+list1[0] = 'x'
+
+print(list1)
+
+list2 = [123, 1.23, True, 'string...']
+
+print(list2)
+
+list3 = [123, 'abcd', ['e1', 'e2', 'e3']]
+
+print(len(list3))  # 3
+
