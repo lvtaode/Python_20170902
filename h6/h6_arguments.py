c92b93db973679715b383772315de47f793db5d7
thu <mingfei.net@gmail.com>
2018-06-12 21:56:54 +0800

    Update

--- h6/h6_arguments.py
+++ h6/h6_arguments.py
@@ -1,6 +1,6 @@
 # argument parameter 参数
 
-# 位置参数
+# 1. 位置参数
 
 # def power(x):
 #     return x * x
@@ -11,9 +11,70 @@
 def cube(x):
     return x * x * x
 
+
 print(cube(2))
 
+
 def power(x, n):
-    # ?
+    p = 1
+    while (n > 0):
+        p = p * x
+        n -= 1
+    return p
+
+
+print(power(2, 2))
+print(power(2, 3))
+print(power(3, 4))
+
+print(power(1234, 1234))
+
+
+# 2. 默认参数
+
+def power2(x, n=2):
+    p = 1
+    while (n > 0):
+        p = p * x
+        n -= 1
+    return p
+
+
+print(power2(2, 3))
+print(power2(2, 2))
+print(power2(2))
+
+
+def power3(x, n=2):
+    p = 1
+    while (n > 0):
+        p = p * x
+        n -= 1
+    return p
+
+
+def print_student_info(name, age, gender='Male', loc='Beijing'):
+    print(name, age, gender, loc)
+    return
+
+
+print(print_student_info('Tom', 18))
+print(print_student_info('Tom', 18, 'Female'))
+print(print_student_info('Tom', 18, 'Female', 'Shanghai'))
+
+print(print_student_info('Tom', 18, loc='Shanghai'))  # ?
+
+# 3. 可变参数
+print(max(-1, -2, -3, .4567))
+
+
+def sum(*n):
+    s = 0
+    for i in n:
+        s += i
+    return s
+
+
+print(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
 
